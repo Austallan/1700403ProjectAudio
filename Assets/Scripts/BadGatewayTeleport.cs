4c4b4db1b8abd5b3ce7b402657d1eaa32c2de7cf
Austallan <47108442+Austallan@users.noreply.github.com>
2020-12-22 22:48:48 +0000

    Teleport maze works
    Took 3 hours to find out why the VERY SIMPLE teleport code wasn't working. Note to anyone who reads this, the Character Controller won't let you teleport it so you have to turn it off, teleport it, then turn it back on

---
+++ Assets/Scripts/BadGatewayTeleport.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadGatewayTeleport : MonoBehaviour
+{
+	public Transform teleportTarget;
+	public GameObject thePlayer;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		CharacterController cc = thePlayer.GetComponent<CharacterController>();
+		cc.enabled = false;
+
+		Debug.Log("Something should teleport now");
+		thePlayer.transform.position = teleportTarget.transform.position;
+		Debug.Log("Something should have been teleported now");
+
+		cc.enabled = true;
+	}
+}
